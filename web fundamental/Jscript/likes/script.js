3e3a9841687395dc8d74f68efc83c595e3a8fc4c
odai sheikh <odaior300210@gmail.com>
2022-01-16 18:42:10 +0200

    nnbn

---
+++ web fundamental/Jscript/likes/script.js
@@ -0,0 +1,18 @@
+var counter = 0;
+function like(){
+    console.log("8");
+    counter++;
+    document.querySelector(".last-l").innerHTML=counter;
+}
+var counter1 = 0;
+function like1(){
+    console.log("8");
+    counter1++;
+    document.querySelector(".last-1").innerHTML=counter1;
+}
+var counter2 = 0;
+function like2(){
+    console.log("8");
+    counter2++;
+    document.querySelector(".last-2").innerHTML=counter2;
+}
