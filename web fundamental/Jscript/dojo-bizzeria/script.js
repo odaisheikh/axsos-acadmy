23553c6502a52a7d603df7bd09f87b0aacd864a0
odai sheikh <odaior300210@gmail.com>
2022-01-16 16:23:28 +0200

    fsf

--- web fundamental/Jscript/dojo-bizzeria/script.js
+++ web fundamental/Jscript/dojo-bizzeria/script.js
@@ -1,12 +1,14 @@
 function pizzaoven(crustType, sauceType, cheeses, toppings) {
     var pizza = {};
-    pizzaoven.crustType = crustType;
-    pizzaoven.sauceType = sauceType;
-    pizzaoven.cheese = cheeses;
-    pizzaoven.toppings = toppings;
+    pizza.crustType = crustType;
+    pizza.sauceType = sauceType;
+    pizza.cheeses = cheeses;
+    pizza.toppings = toppings;
     return pizza;
 }
     
 var s1 = pizzaoven("deep dish", "traditional", ["mozzarella"], ["pepperoni", "sausage"]);
 
 console.log(s1);
+var s2 = pizzaoven("hand tossed", "marinaral", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"]);
+console.log(s2);
