73b5641e3bfdaaa02c4b4519900d7e780da416d1
odai sheikh <odaior300210@gmail.com>
2022-01-30 14:11:15 +0200

    make file

--- Python/python/hello-world/hello_world.py
+++ Python/python/hello-world/hello_world.py
@@ -1,3 +1,4 @@
+"""
 print("hassan")
 name = "odai"
 print("hello ", name , "..")
@@ -7,4 +8,15 @@ print("hello"+str(num))
 first_food ,secound_food = "banana" , "mansf"
 print("i love to eat {} and {}".format(first_food,secound_food))
 print(f"i love to eat {first_food} and {secound_food}")
+"""
+y = 3
+while y > 0:
+    print(y)
+    y = y - 1
+    if y == 0:
+        break
+    else:		# only executes on a clean exit from the while loop (i.e. not a break)
+        print("Final else statement")
+# output: 3, 2, 1
+
 
