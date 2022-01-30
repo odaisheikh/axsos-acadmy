253d4601219d4b0cccf79dd947bc0fff9746d679
odai sheikh <odaior300210@gmail.com>
2022-01-30 11:57:38 +0200

    jhjh

---
+++ Python/python/hello-world/hello_world.py
@@ -0,0 +1,10 @@
+print("hassan")
+name = "odai"
+print("hello ", name , "..")
+num = 44
+print("hello",num)
+print("hello"+str(num))
+first_food ,secound_food = "banana" , "mansf"
+print("i love to eat {} and {}".format(first_food,secound_food))
+print(f"i love to eat {first_food} and {secound_food}")
+
