de6dc0956c4dd915a85ecf5dd9d3d468e3e56b3e
odai sheikh <odaior300210@gmail.com>
2022-01-30 16:14:50 +0200

    done

--- Python/python/for_loop_basic1/for_loop_basic1.py
+++ Python/python/for_loop_basic1/for_loop_basic1.py
@@ -15,7 +15,7 @@ for key in range(0,500001):
     if key % 2 != 0:
         sum+=key
 print(sum)
-"""
+
 
 for key in range(2018,0 ,-4):
     print(key)
@@ -23,5 +23,29 @@ for key in range(2018,0 ,-4):
 for key in range(x,y,z):
     if key%z==0:
         print (key)
+"""
+#5
+def a():
+    return(5)
+def a1():
+    return("5")
+print(a()+a1())
+#7
+def a(b,c):
+    return str(b)+str(c)
+print(a(2,5))
+#9
+def a(b,c):
+    if b<c:
+        return 7
+    else:
+        return 14
+    return 3
+print(a(2,3))
+print(a(5,3))
+print(a(2,3) + a(5,3))
+
+
+
 
 
