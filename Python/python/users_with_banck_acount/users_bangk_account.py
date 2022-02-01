29d126ed450baa03d4e07c782396dcdeb813eab7
odai sheikh <odaior300210@gmail.com>
2022-02-01 12:53:27 +0200

    finsh solve

--- Python/python/users_with_banck_acount/users_bangk_account.py
+++ Python/python/users_with_banck_acount/users_bangk_account.py
@@ -14,16 +14,21 @@ class BanckAccount:
         if self.account_balance>0:
             self.account_balance +=self.account_balance*.01
 class User:
-    def __init__(self,name,email,account):
+    def __init__(self,name,email):
         self.name=name
         self.email=email
-        self.account=BanckAccount()
-    def make_deposit(self,amount):
-        self.account.make_deposit(amount) 
-    def withdrow (self,amount):
-        self.account.account_balance -=amount
-    def display_user_balance(self):
-        print(self.name +":"+self.account.account_balance)
-odai= User
-#odai.make_deposit(100)
-odai.display_user_balance()
+        self.account=[BanckAccount()]
+    def add_new_account(self):
+        self.account.append(BanckAccount())
+    def make_deposit(self,amount,account_num):
+        self.account[account_num].make_deposit(amount) 
+    def withdrow (self,amount,account_num):
+        self.account[account_num].account_balance -=amount
+    def display_user_info(self,account_num):
+        print(self.name ," your balance account is : ",self.account[account_num].account_balance )
+odai= User("odai","odaior300210@gmail.com")
+odai.add_new_account()
+odai.make_deposit(1000,1)
+odai.account[1].interest_yeld()
+odai.withdrow(50,1)
+print(odai.account[1].account_balance)
