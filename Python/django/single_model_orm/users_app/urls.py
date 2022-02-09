e58610d8c5debf3233e949e15a3b1b047e5a381f
odai sheikh <odaior300210@gmail.com>
2022-02-09 15:43:02 +0200

    assiment

--- Python/django/single_model_orm/users_app/urls.py
+++ Python/django/single_model_orm/users_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path     
 from . import views
 urlpatterns = [
-    # path('', views.index),	   
+    path('', views.form),
+    path('add_users', views.handel)	   
 ]
--- Python/django/single_model_orm/users_app/views.py
+++ Python/django/single_model_orm/users_app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from multiprocessing import context
+from django.shortcuts import render,redirect
+from .models import users
+def form(request):
+    print("*")
+    context = {
+        'user':users.objects.all()
+    }
+    return render(request,"index.html" ,context)
 
-# Create your views here.
+def handel(request):
+    print("*")
+    users.objects.create(first_name = request.POST['first_name'],lastname_name = request.POST['last_name']
+    ,email_address = request.POST['email'],age = request.POST['age'])
+    return redirect ('/')
