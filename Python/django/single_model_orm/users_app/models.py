0a5c9723364979691a0f05f8fee7160e14889a0d
odai sheikh <odaior300210@gmail.com>
2022-02-11 16:51:38 +0200

    assiment

--- Python/django/single_model_orm/users_app/models.py
+++ Python/django/single_model_orm/users_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-class users(models.Model):
+class User(models.Model):
     first_name = models.CharField(max_length=255)
     lastname_name = models.CharField(max_length=255)
     email_address = models.CharField(max_length=255)
--- Python/django/single_model_orm/users_app/views.py
+++ Python/django/single_model_orm/users_app/views.py
@@ -1,15 +1,15 @@
 from multiprocessing import context
 from django.shortcuts import render,redirect
-from .models import users
+from .models import User
 def form(request):
     print("*")
     context = {
-        'user':users.objects.all()
+        'user':User.objects.all()
     }
     return render(request,"index.html" ,context)
 
 def handel(request):
     print("*")
-    users.objects.create(first_name = request.POST['first_name'],lastname_name = request.POST['last_name']
+    User.objects.create(first_name = request.POST['first_name'],lastname_name = request.POST['last_name']
     ,email_address = request.POST['email'],age = request.POST['age'])
     return redirect ('/')
