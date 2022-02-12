9b27cbce38acf25820721304140bc5f04f20ace7
odai sheikh <odaior300210@gmail.com>
2022-02-12 17:55:02 +0200

    compleate assiment

--- Python/django/dojo_ninja_project/dojo_ninja_app/urls.py
+++ Python/django/dojo_ninja_project/dojo_ninja_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path     
 from . import views
 urlpatterns = [
-    path('', views.index)	   
+    path('', views.index),
+    path('handel', views.handel),
+    path('handel1', views.handel1),   
 ]
--- Python/django/dojo_ninja_project/dojo_ninja_app/views.py
+++ Python/django/dojo_ninja_project/dojo_ninja_app/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render,HttpResponse
-
+from multiprocessing import context
+from unicodedata import name
+from django.shortcuts import render,HttpResponse,redirect
+from .models import Dojo,Ninja
 def index(request):
-    return HttpResponse("hello")
+    context = {
+        'dojos' : Dojo.objects.all()
+    }
+    return render(request,"index.html" , context)
+def handel(request):
+    Dojo.objects.create(name = request.POST['name'],city = request.POST['city'],state = request.POST['state'])
+    return redirect("/","index.html")
+def handel1(request):
+    Ninja.objects.create(first_name = request.POST['first_name'],last_name = request.POST['last_name'],dojo = Dojo.objects.get(id=request.POST['dojo']))
+    print(request.POST)
+    return redirect('/',"index.html")
