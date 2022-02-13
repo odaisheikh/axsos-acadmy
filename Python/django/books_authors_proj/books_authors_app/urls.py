8e68ce96e6c89a41ce41abbe8321e0ec294d839a
odai sheikh <odaior300210@gmail.com>
2022-02-13 17:46:21 +0200

    assiment

--- Python/django/books_authors_proj/books_authors_app/urls.py
+++ Python/django/books_authors_proj/books_authors_app/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path     
 from . import views
 urlpatterns = [
-    path('',views.index)
+    path('add_book/',views.add_app),
+    path('form_add',views.form_add),
+    path('show/<int:book_id>',views.view ,name='show'),
+    path('add_author',views.add_author),
+
 ]
--- Python/django/books_authors_proj/books_authors_app/views.py
+++ Python/django/books_authors_proj/books_authors_app/views.py
@@ -1,6 +1,35 @@
 from http.client import HTTPResponse
-from django.shortcuts import render,HttpResponse
+from multiprocessing import context
+from django.shortcuts import render,HttpResponse,redirect
+from .models import Book,Author
 
+def add_app(request):
+    context = {
+        'book': Book.objects.all
+    }
+    return render(request , "add_book.html" ,context)
 
-def index(request):
-    return HttpResponse("hello")
+def form_add(request):
+    Book.objects.create(title = request.POST['title'],desc = request.POST['description'] )
+    return redirect("/add_book")
+
+def view(request,book_id):
+    print('***')
+    book1 = Book.objects.get(id=book_id)
+    this_book = Book.objects.get(id=book_id)
+    context = {
+        'book':Book.objects.get(id=book_id),
+        # 'authors_book': Author.objects.filter(books=book1),
+        'authors':this_book.authors.all,
+        'authors_all':Author.objects.all,
+        'authors_all':Author.objects.all
+    }
+    return render(request,'view.html' ,context)
+
+def add_author(request):
+    print("********")
+    print(request)
+    this_book = Book.objects.get(id=int(request.POST['book_id']))
+    this_author = Author.objects.get(id=int(request.POST['auth_id']))
+    this_book.authors.add(this_author)
+    return redirect("/show/" + request.POST['book_id'])
