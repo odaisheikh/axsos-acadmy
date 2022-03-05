ad536192530d5efc3dad565eb5397d27fdfddbf6
odai sheikh <odaior300210@gmail.com>
2022-03-05 14:46:02 +0200

    assiment

---
+++ java/fundamental/first_java_program/src/first_java_program/First_java_program.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package first_java_program;
+
+/**
+ *
+ * @author user
+ */
+public class First_java_program {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String name  = "odai";
+        int age = 23;
+        String hometown = "qalqilya palestine";
+        System.out.println("my name is "+name);
+        System.out.println("my age is "+age);
+        System.out.println("my hometown is "+hometown);
+        
+    }
+    
+}
