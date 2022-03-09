f6662126b10e692bc380b331510695b25103b301
odai sheikh <odaior300210@gmail.com>
2022-03-09 22:41:02 +0200

    reorder filrs

---
+++ java/OOP/itemOrder/Order.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+public class Order {
+    private String name;
+    Item x = new Item();
+    // private double total;
+    private boolean ready ;
+    private ArrayList<Item> items  ;
+
+public Order(){
+    this.name = "Geust";
+    items = new ArrayList<Item>();
+}
+public Order(String name){
+    this.name = name;
+    // this.ready = ready;
+    // this.total = total;
+    // this.items = items;
+}
+public void setName(String name){
+    this.name = name;
+}
+public void setItems(Item item){
+    this.items.add(item);
+}
+// public void Price(double price){
+//     this.total = price;
+// }
+public void setReady(boolean ready){
+    this.ready = ready;
+}
+public boolean isReady(){
+    return this.ready;
+}
+public String getName(String name){
+    return this.name;
+}
+// public double getTotal(double total){
+//     return this.total;
+// }
+public ArrayList<Item> getItem(){
+    return this.items;
+}
+public String getStatusMessage(){
+    if (ready){
+        return "your order is ready";
+    }
+    else{ 
+    return "yore order will be ready soon";}
+}
+public void display(){
+    double total = 0;
+    System.out.println(this.name);
+    for(Item x : items){
+        total += x.getPrice();
+        System.out.println(x.getName() + "price :"+ x.getPrice());
+    }
+    System.out.println(total);
+}
+
+}
---
+++ java/OOP/itemOrder/TestOrder.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class TestOrder{
+
+    public static void main(String[] args){
+        // Item item1 = new Item();
+        // Item item2 = new Item();
+        // Item item3 = new Item();
+        // Item item4 = new Item();
+        // item1.setName("mocha"); 
+        // item2.setName("latte");
+        // item3.setName("drip"); 
+        // item4.setName("capuccino"); 
+        // item1.setPrice(30); 
+        // item2.setPrice(300); 
+        // item3.setPrice(330); 
+        // item4.setPrice(310);
+        // Order order1 = new Order();
+        // order1.setItems(item1);
+        // order1.setItems(item2);
+        // order1.display();
+        // order1.setReady(true);
+        // // System.out.println(order1.getItem().getName());
+        // ArrayList <Item> xc ;
+        // xc = order1.getItem();
+        // for(Item x : xc){
+        //     System.out.println(x.getName());
+        // }
+        // System.out.println(order1.getStatusMessage()); 
+
+        NewOrder x = new NewOrder();
+        x.addMenu("cafe", 11);
+        x.addMenu("tea", 12);
+        x.addMenu("milk", 120);
+        x.newOrder();
+
+    }
+}
