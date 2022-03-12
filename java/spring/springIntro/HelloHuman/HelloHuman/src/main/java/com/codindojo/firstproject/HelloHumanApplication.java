803f895ca9f12e803142c2ced86ab13da6e9d948
odai sheikh <odaior300210@gmail.com>
2022-03-12 13:42:00 +0200

    some changes to the assiment

---
+++ java/spring/springIntro/HelloHuman/HelloHuman/src/main/java/com/codindojo/firstproject/HelloHumanApplication.java
@@ -0,0 +1,24 @@
+package com.codindojo.firstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+
+public class HelloHumanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloHumanApplication.class, args);
+	}
+	@RequestMapping("/")
+	public String index(@RequestParam(value="q", required=false ,defaultValue = "human") String First_name, @RequestParam(value = "last_name",required = false ,defaultValue = "" )
+	String last_name, @RequestParam(value="times",required = false,defaultValue = "1") String times) {
+		return "hello " + (First_name +" "+ last_name).repeat(Integer.valueOf(times));
+	}
+	
+
+}
